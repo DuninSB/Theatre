4ab67acb9c17d044e4d02f417796d6971ad63edb
DuninSB <cs-1@bk.ru>
2024-12-18 20:29:53 +0300

    Educational project for the creation of a website for theatre

---
+++ homepage/views.py
@@ -0,0 +1,19 @@
+
+from django.views.generic import ListView
+
+from plays.models import Show
+
+
+
+# Create your views here.
+class HomepageView(ListView):
+    template_name = "homepage.html"
+    model = Show
+
+    def get_queryset(self):
+        return Show.objects.active()
+
+
+
+
+
---
+++ plays/models.py
@@ -0,0 +1,35 @@
+from operator import truediv
+
+from django.db import models
+from django.contrib.admin import display
+from django.urls import reverse
+from django.db.models import functions
+
+# Create your models here.
+class Play(models.Model):
+    title = models.CharField(max_length=255)
+    annotation = models.TextField()
+
+class Actor(models.Model):
+    name = models.CharField(max_length=255)
+    birthday = models.DateField(null=True)
+
+class ShowManager(models.Manager):
+    def active(self):
+        return self.get_queryset().filter(starts_at__gt=functions.Now())
+
+
+class Show(models.Model):
+    starts_at =models.DateTimeField()
+    play = models.ForeignKey(Play, on_delete=models.CASCADE)
+    actors = models.ManyToManyField(Actor)
+
+    objects = ShowManager()
+
+    @display
+    def play_name(self):
+        return self.play.title
+
+    def get_absolute_url(self):
+        return reverse("show_detail", kwargs={"pk": self.pk})
+
